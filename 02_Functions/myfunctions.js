162285761ff58f398bac07f9b35c1add169b6ee8
gajurelkshitiz <kshitiz.purbct042@gmail.com>
2025-09-18 16:58:58 +0545

    arrow and normal func

--- 02_Functions/myfunctions.js
+++ 02_Functions/myfunctions.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 function addTwo(num) {
     return num + 2;
+    // return "hello"   
 }
 function getUpper(val) {
     val.toUpperCase();
@@ -11,6 +12,24 @@ function signUpUser(name, email, isPaid) {
 function loginUser(name, email, isPaid) {
     if (isPaid === void 0) { isPaid = false; }
 }
+// traditional function
+function getValue(myVal) {
+    if (myVal > 5) {
+        return true;
+    }
+    return "200 OK";
+}
+getValue(5);
+// arrow function
+var getHello = function (s) {
+    return "";
+};
+var heros = ["thor", "spiderman", "ironman"]; // hero is string by default: it senses.
+// const heros = [1, 2, 3]
+heros.map(function (hero) {
+    return "hero is ".concat(hero);
+    // return 2   // not allowed
+});
 addTwo(5);
 getUpper("hello");
 signUpUser("abc", "abc@gmail.com", false);
--- 02_Functions/myfunctions.ts
+++ 02_Functions/myfunctions.ts
@@ -16,6 +16,34 @@ function loginUser(name: string, email: string, isPaid: boolean = false) {
 }
 
 
+
+// traditional function
+function getValue(myVal: number) {
+    if(myVal>5) {
+        return true
+    }
+    return "200 OK"
+}
+
+getValue(5)
+
+
+// arrow function
+const getHello = (s: string): string => {
+    return ""
+}
+
+
+
+const heros = ["thor","spiderman","ironman"]    // hero is string by default: it senses.
+// const heros = [1, 2, 3]
+
+heros.map((hero): string => {
+    return `hero is ${hero}`
+    // return 2   // not allowed
+})
+
+
 addTwo(5)
 
 getUpper("hello")
