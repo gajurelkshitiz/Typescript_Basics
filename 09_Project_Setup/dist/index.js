9fa96969f71e44cff394436600675764d879db66
gajurelkshitiz <kshitiz.purbct042@gmail.com>
2025-09-25 13:41:45 +0545

    getter and setter in typescript

--- 09_Project_Setup/dist/index.js
+++ 09_Project_Setup/dist/index.js
@@ -1,12 +1,40 @@
 "use strict";
 console.log("First Typescript Project Setup, hello world");
 console.log('typescript is amazing');
-// class:
+// <--------------- CLASS DEFINITION --------------------->:
+// class User {
+//     public email: string
+//     private name: string
+//     readonly city: string = "Biratnagar"
+//     constructor(email: string, name: string) {
+//         this.email = email;
+//         this.name = name;  
+//         this.city       // this is allowed for private, readonly
+//     }
+// }
+// <--------------- PROFESSIONALLY DEFINING CLASSES --------------------->:
 class User {
     constructor(email, name) {
-        this.city = "";
         this.email = email;
         this.name = name;
+        this._courseCount = 1;
+        this.city = "Biratnagar";
+    }
+    deleteToken() {
+        console.log('Token Deleted');
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum;
     }
 }
 const kshitiz = new User("kshitiz@gmail.com", "kshitiz");
+kshitiz.city; // it is readable is i use 'readonly city: string = "Biratnagar" '
--- 09_Project_Setup/src/index.ts
+++ 09_Project_Setup/src/index.ts
@@ -2,15 +2,50 @@ console.log("First Typescript Project Setup, hello world");
 console.log('typescript is amazing');
 
 
-// class:
+// <--------------- CLASS DEFINITION --------------------->:
+// class User {
+//     public email: string
+//     private name: string
+//     readonly city: string = "Biratnagar"
+//     constructor(email: string, name: string) {
+//         this.email = email;
+//         this.name = name;  
+//         this.city       // this is allowed for private, readonly
+//     }
+// }
+
+// <--------------- PROFESSIONALLY DEFINING CLASSES --------------------->:
 class User {
-    public email: string
-    private name: string
+
+    private _courseCount = 1;
+
     readonly city: string = "Biratnagar"
-    constructor(email: string, name: string) {
-        this.email = email;
-        this.name = name;  
-        this.city       // this is allowed for private, readonly
+    constructor(
+        public email: string, 
+        public name: string,
+        // private userId: string,
+    ) {
+     
+    }
+
+    private deleteToken() {
+        console.log('Token Deleted');
+    }
+
+
+    get getAppleEmail(): string {
+        return `apple${this.email}`
+    }
+
+    get courseCount(): number {
+        return this._courseCount
+    }
+
+    set courseCount(courseNum) {   // a set accessor can't have a return type annotator.
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum
     }
 }
 
